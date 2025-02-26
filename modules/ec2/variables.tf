959a1032f35886af2976295237ddfa34dafbd578
이성민 <l_keon@iseongmin-ui-MacBookPro.local>
2025-02-26 23:19:46 +0900

    update-tf-code

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,20 +1,23 @@
 variable "ami_id" {
   description = "EC2 인스턴스의 AMI ID"
   type        = string
+  default = "ami-024ea438ab0376a47"
 }
 
 variable "instance_type" {
   description = "EC2 인스턴스 타입"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.small"
 }
 
 variable "subnet_id" {
   description = "EC2 인스턴스가 생성될 서브넷 ID"
   type        = string
+  default = aws_subnet.public.id
 }
 
 variable "environment" {
   description = "환경 이름 (예: dev, prod)"
   type        = string
+  default = "prod"
 } 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,23 +1,23 @@
 variable "vpc_cidr" {
-  description = "10.0.0.0/20"
+  default = "10.0.0.0/20"
   type        = string
 }
 
 variable "environment" {
-  description = "flooding"
+  default = "flooding"
   type        = string
 }
 
 variable "public_subnet_cidr" {
-  description = "10.0.1.0/24"
+  default = "10.0.1.0/24"
   type        = string
 } 
 
 variable "azs" {
-  description = "ap-northeast-2"
+  default = "ap-northeast-2"
   type = string
 }
 variable "private_subnet_cidr" {
-    description = "10.0.2.0/24"
+    default = "10.0.2.0/24"
     type = string
 }
