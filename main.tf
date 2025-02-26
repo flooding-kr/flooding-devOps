3cc5204944b584da90edf2638224659158d2f022
이성민 <l_keon@iseongmin-ui-MacBookPro.local>
2025-02-27 01:23:55 +0900

    finish sg tf

--- main.tf
+++ main.tf
@@ -16,26 +16,26 @@ module "sg" {
   source = "./modules/sg"
 
   environment = "flooding"
-  vpc_id     = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 }
 
 module "ec2_public" {
   source = "./modules/ec2_public"
 
 
-  ami_id        = "ami-024ea438ab0376a47" # Ubuntu 24.04 LTS (서울 리전)
-  instance_type = "t3.small"
-  subnet_id     = module.vpc.public_subnet_id
-  environment   = "flooding"
+  ami_id                 = "ami-024ea438ab0376a47" # Ubuntu 24.04 LTS (서울 리전)
+  instance_type          = "t3.small"
+  subnet_id              = module.vpc.public_subnet_id
+  environment            = "flooding"
   vpc_security_group_ids = [module.sg.server_sg_id]
 }
 
 module "ec2_private" {
   source = "./modules/ec2_private"
 
-  ami_id        = "ami-024ea438ab0376a47"
-  instance_type = "t3.samll"
-  subnet_id     = module.vpc.private_subnet_id
-  environment   = "flooding"
+  ami_id                 = "ami-024ea438ab0376a47"
+  instance_type          = "t3.samll"
+  subnet_id              = module.vpc.private_subnet_id
+  environment            = "flooding"
   vpc_security_group_ids = [module.sg.db_sg_id]
 }
