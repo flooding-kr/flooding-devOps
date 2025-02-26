ea50a9736767b0a6bc106551d57d08bc623860fe
이성민 <l_keon@iseongmin-ui-MacBookPro.local>
2025-02-26 22:57:02 +0900

    update-module

--- main.tf
+++ main.tf
@@ -5,16 +5,18 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
   
-  vpc_cidr           = "10.0.0.0/16"
-  environment        = "dev"
+  vpc_cidr           = "10.0.0.0/20"
+  azs = [ "ap-northeast-2a" ]
+  environment        = "prod"
   public_subnet_cidr = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
 }
 
 module "ec2" {
   source = "./modules/ec2"
 
-  ami_id       = "ami-0c9c942bd7bf113a2"  # Amazon Linux 2 AMI ID (서울 리전)
-  instance_type = "t2.micro"
+  ami_id       = "ami-024ea438ab0376a47"  # Ubuntu 24.04 LTS (서울 리전)
+  instance_type = "t3.small"
   subnet_id     = module.vpc.public_subnet_id
-  environment   = "dev"
+  environment   = "prod"
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,12 +12,26 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
+  availability_zone = [var.azs]
   cidr_block = var.public_subnet_cidr
 
   map_public_ip_on_launch = true
 
   tags = {
     Name        = "${var.environment}-public-subnet"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  availability_zone = [var.azs]
+  cidr_block = var.private_subnet_cidr
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.environment}-private-subnet"
     Environment = var.environment
   }
 }
@@ -41,11 +55,28 @@ resource "aws_route_table" "public" {
 
   tags = {
     Name        = "${var.environment}-public-rt"
-    Environment = var.environment
   }
 }
+
 
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 } 
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+  }
+
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
